5370718b7496de7be64dd84965bc7385b3df845e
Jordi Ribellas <ribellas2002@gmail.com>
2022-12-21 19:14:46 +0100

    DESENCRIPTAR FET

--- src/codificacionEnigma.java
+++ src/codificacionEnigma.java
@@ -125,7 +125,7 @@ public class codificacionEnigma extends JFrame{
 				case 2:
 					int ascii2 = (int) chars[i];
 					
-					if((ascii2 + num1)>=90) {
+					if((ascii2 + num2)>=90) {
 						restador = ascii2 + num2;
 						restador = restador - ascii2;
 						ascii2 = codi + restador;
@@ -137,7 +137,7 @@ public class codificacionEnigma extends JFrame{
 				case 3:
 					int ascii3 = (int) chars[i];
 					
-					if((ascii3 + num1)>=90) {
+					if((ascii3 + num3)>=90) {
 						restador = ascii3 + num3;
 						restador = restador - ascii3;
 						ascii3 = codi + restador;
@@ -162,7 +162,63 @@ public class codificacionEnigma extends JFrame{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			int cont = 1;
+			String text = entrada.getText();
+			char[] chars = text.toCharArray();
+			char[] solucio = new char[chars.length];
 			
+			int num1 = Integer.parseInt(codificacio1.getText());
+			int num2 = Integer.parseInt(codificacio2.getText());
+			int num3 = Integer.parseInt(codificacio3.getText());
+			
+			int restador, codi=91;
+			
+			for(int i=0; i<text.length(); i++) {
+				
+				switch (cont) {
+				case 1:
+					int ascii1 = (int) chars[i];
+					
+					if((ascii1 - num1)<=65) {
+						restador = ascii1 - num1;
+						restador = restador - ascii1;
+						ascii1 = codi + restador;
+					}
+					solucio[i] = (char) ascii1;
+					cont = 2;
+					break;
+					
+				case 2:
+					int ascii2 = (int) chars[i];
+					
+					if((ascii2 - num2)<=65) {
+						restador = ascii2 - num2;
+						restador = restador - ascii2;
+						ascii2 = codi + restador;
+					}
+					solucio[i] = (char) ascii2;
+					cont = 3;
+					break;
+					
+				case 3:
+					int ascii3 = (int) chars[i];
+					
+					if((ascii3 - num3)<=65) {
+						restador = ascii3 - num3;
+						restador = restador - ascii3;
+						ascii3 = codi + restador;
+					}
+					solucio[i] = (char) ascii3;
+					cont = 1;
+					break;
+
+				default:
+					break;
+				}
+			}
+			
+			String str = new String(solucio);
+			sortida.setText(str);
 			
 		}
 		
