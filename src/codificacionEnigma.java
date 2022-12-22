78bf47cdcd8d4905b5b87c01315ed92259120eab
Jordi Ribellas <ribellas2002@gmail.com>
2022-12-22 22:03:19 +0100

    final + docu

--- src/codificacionEnigma.java
+++ src/codificacionEnigma.java
@@ -3,6 +3,8 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,231 +13,271 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-public class codificacionEnigma extends JFrame{
-	
+public class codificacionEnigma extends JFrame {
+
 	JPanel original = new JPanel();
 	JPanel codificat = new JPanel();
 	JPanel botons = new JPanel();
 	JPanel resultat = new JPanel();
 	Container container = getContentPane();
-	
+
 	JTextField entrada = new JTextField();
 	JTextField sortida = new JTextField();
-	
+
 	JTextField codificacio1 = new JTextField(3);
 	JTextField codificacio2 = new JTextField(3);
 	JTextField codificacio3 = new JTextField(3);
-	
+
+	Boolean error = false;
+
 	public codificacionEnigma() {
-		
-        super("Enigma");
-        setSize(800, 300);
-        
-        GridLayout gl = new GridLayout(4,1);
-        
-        //Assignem un Layout a cada Panel
-        original.setLayout(new GridLayout(2,1));        
-        codificat.setLayout(new FlowLayout());
-        botons.setLayout(new FlowLayout());
-        resultat.setLayout(new GridLayout(2,1));
-        
-        //Afegim que volem que hi hagui a cada Panel
-        JLabel label1 = new JLabel("Escriu el missatge:");
-        label1.setHorizontalAlignment(SwingConstants.CENTER);
-        original.add(label1);
-        
-        
-        entrada.setHorizontalAlignment(SwingConstants.CENTER);
-        original.add(entrada);
-        
-        
-        JLabel text = new JLabel("Escriu els numeros que codificaran:");
-        text.setHorizontalAlignment(SwingConstants.CENTER);
-        codificat.add(text);
-                
-        
-        codificacio1.setHorizontalAlignment(SwingConstants.CENTER);
-        codificat.add(codificacio1);
-        
-        
-        codificacio2.setHorizontalAlignment(SwingConstants.CENTER);
-        codificat.add(codificacio2);
-        
-        
-        codificacio3.setHorizontalAlignment(SwingConstants.CENTER);
-        codificat.add(codificacio3);
-        
-        
-        JButton encriptar = new JButton("Encriptar");
-        encriptar.addActionListener(new encript());
-        
-        JButton desencriptar = new JButton("Desencriptar");
-        desencriptar.addActionListener(new decript());
-        
-        botons.add(encriptar);
-        botons.add(desencriptar);
-        
-        JLabel label2 = new JLabel("Aquest es el resultat");
-        label2.setHorizontalAlignment(SwingConstants.CENTER);
-        resultat.add(label2);
-        
-        sortida.setHorizontalAlignment(SwingConstants.CENTER);
-        sortida.setEditable(false);
-        resultat.add(sortida);
-		
-        //Ho afegim al container
-        container.setLayout(gl);
-        container.add(original);
-        container.add(codificat);
-        container.add(botons);
-        container.add(resultat);
-        
+
+		super("Enigma");
+		setSize(800, 300);
+
+		GridLayout gl = new GridLayout(4, 1);
+
+		// Assignem un Layout a cada Panel
+		original.setLayout(new GridLayout(2, 1));
+		codificat.setLayout(new FlowLayout());
+		botons.setLayout(new FlowLayout());
+		resultat.setLayout(new GridLayout(2, 1));
+
+		// Afegim que volem que hi hagui a cada Panel
+		JLabel label1 = new JLabel("Escriu el missatge:");
+		label1.setHorizontalAlignment(SwingConstants.CENTER);
+		original.add(label1);
+
+		entrada.setHorizontalAlignment(SwingConstants.CENTER);
+		original.add(entrada);
+
+		JLabel text = new JLabel("Escriu els numeros que codificaran:");
+		text.setHorizontalAlignment(SwingConstants.CENTER);
+		codificat.add(text);
+
+		codificacio1.setHorizontalAlignment(SwingConstants.CENTER);
+		codificat.add(codificacio1);
+
+		codificacio2.setHorizontalAlignment(SwingConstants.CENTER);
+		codificat.add(codificacio2);
+
+		codificacio3.setHorizontalAlignment(SwingConstants.CENTER);
+		codificat.add(codificacio3);
+
+		JButton encriptar = new JButton("Encriptar");
+		encriptar.addActionListener(new encript());
+
+		JButton desencriptar = new JButton("Desencriptar");
+		desencriptar.addActionListener(new decript());
+
+		botons.add(encriptar);
+		botons.add(desencriptar);
+
+		JLabel label2 = new JLabel("Aquest es el resultat");
+		label2.setHorizontalAlignment(SwingConstants.CENTER);
+		resultat.add(label2);
+
+		sortida.setHorizontalAlignment(SwingConstants.CENTER);
+		sortida.setEditable(false);
+		resultat.add(sortida);
+
+		// Ho afegim al container
+		container.setLayout(gl);
+		container.add(original);
+		container.add(codificat);
+		container.add(botons);
+		container.add(resultat);
+
 	}
-	
-	public class encript implements ActionListener{
+
+	public class encript implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			error = false;
+
 			int cont = 1;
 			String text = entrada.getText();
 			char[] chars = text.toCharArray();
 			char[] solucio = new char[chars.length];
-			
+
 			int num1 = Integer.parseInt(codificacio1.getText());
 			int num2 = Integer.parseInt(codificacio2.getText());
 			int num3 = Integer.parseInt(codificacio3.getText());
-			
-			int restador=0, codi=90, tope=64;
-			
-			for(int i=0; i<text.length(); i++) {
-				restador=0;
-				
-				switch (cont) {
-				case 1:
-					int ascii1 = (int) chars[i];
-					
-					if((ascii1 + num1)>90) {
-						restador = ascii1 + num1;
-						restador = restador - codi;
-						ascii1 = tope + restador;
-					}else{
-						ascii1 = ascii1 + num1;
-					}
-					solucio[i] = (char) ascii1;
-					cont = 2;
-					break;
-					
-				case 2:
-					int ascii2 = (int) chars[i];
-					
-					if((ascii2 + num2)>90) {
-						restador = ascii2 + num2;
-						restador = restador - codi;
-						ascii2 = tope + restador;
-					}else{
-						ascii2 = ascii2 + num2;
-					}
-					solucio[i] = (char) ascii2;
-					cont = 3;
-					break;
-					
-				case 3:
-					int ascii3 = (int) chars[i];
-					
-					if((ascii3 + num3)>90) {
-						restador = ascii3 + num3;
-						restador = restador - codi;
-						ascii3 = tope + restador;
-					}else{
-						ascii3 = ascii3 + num3;
-					}
-					solucio[i] = (char) ascii3;
-					cont = 1;
-					break;
 
-				default:
-					break;
+			int restador = 0, codi = 90, tope = 64;
+
+			// cridar la funcio de comprovar caracters
+			comprovarCaracters(chars);
+
+			if (!error) {
+				for (int i = 0; i < text.length(); i++) {
+					restador = 0;
+
+					switch (cont) {
+					case 1:
+						int ascii1 = (int) chars[i];
+
+						if ((ascii1 + num1) > 90) {
+							restador = ascii1 + num1;
+							restador = restador - codi;
+							ascii1 = tope + restador;
+						} else {
+							ascii1 = ascii1 + num1;
+						}
+						solucio[i] = (char) ascii1;
+						cont = 2;
+						break;
+
+					case 2:
+						int ascii2 = (int) chars[i];
+
+						if ((ascii2 + num2) > 90) {
+							restador = ascii2 + num2;
+							restador = restador - codi;
+							ascii2 = tope + restador;
+						} else {
+							ascii2 = ascii2 + num2;
+						}
+						solucio[i] = (char) ascii2;
+						cont = 3;
+						break;
+
+					case 3:
+						int ascii3 = (int) chars[i];
+
+						if ((ascii3 + num3) > 90) {
+							restador = ascii3 + num3;
+							restador = restador - codi;
+							ascii3 = tope + restador;
+						} else {
+							ascii3 = ascii3 + num3;
+						}
+						solucio[i] = (char) ascii3;
+						cont = 1;
+						break;
+
+					default:
+						break;
+					}
 				}
 			}
-			
+
 			String str = new String(solucio);
 			sortida.setText(str);
-			
+
 		}
-		
+
 	}
-	
-	public class decript implements ActionListener{
+
+	public class decript implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			error = false;
+
 			int cont = 1;
 			String text = entrada.getText();
 			char[] chars = text.toCharArray();
 			char[] solucio = new char[chars.length];
-			
+
 			int num1 = Integer.parseInt(codificacio1.getText());
 			int num2 = Integer.parseInt(codificacio2.getText());
 			int num3 = Integer.parseInt(codificacio3.getText());
-			
-			int restador=0, codi=65, tope=91;
-			
-			for(int i=0; i<text.length(); i++) {
-				restador=0;
-				
-				switch (cont) {
-				case 1:
-					int ascii1 = (int) chars[i];
-					
-					if((ascii1 - num1)<65) {
-						restador = ascii1 - num1;
-						restador = codi - restador;
-						ascii1 = tope - restador;
-					}else{
-						ascii1 = ascii1 - num1;
-					}
-					solucio[i] = (char) ascii1;
-					cont = 2;
-					break;
-					
-				case 2:
-					int ascii2 = (int) chars[i];
-					
-					if((ascii2 - num2)<65) {
-						restador = ascii2 - num2;
-						restador = codi - restador;
-						ascii2 = tope - restador;
-					}else{
-						ascii2 = ascii2 - num2;
-					}
-					solucio[i] = (char) ascii2;
-					cont = 3;
-					break;
-					
-				case 3:
-					int ascii3 = (int) chars[i];
-					
-					if((ascii3 - num3)<65) {
-						restador = ascii3 - num3;
-						restador = codi - restador;
-						ascii3 = tope - restador;
-					}else{
-						ascii3 = ascii3 - num3;
-					}
-					solucio[i] = (char) ascii3;
-					cont = 1;
-					break;
 
-				default:
-					break;
+			int restador = 0, codi = 65, tope = 91;
+
+			// cridar la funcio de comprovar caracters
+			comprovarCaracters(chars);
+
+			if (!error) {
+				for (int i = 0; i < text.length(); i++) {
+					restador = 0;
+
+					switch (cont) {
+					case 1:
+						int ascii1 = (int) chars[i];
+
+						if ((ascii1 - num1) < 65) {
+							restador = ascii1 - num1;
+							restador = codi - restador;
+							ascii1 = tope - restador;
+						} else {
+							ascii1 = ascii1 - num1;
+						}
+						solucio[i] = (char) ascii1;
+						cont = 2;
+						break;
+
+					case 2:
+						int ascii2 = (int) chars[i];
+
+						if ((ascii2 - num2) < 65) {
+							restador = ascii2 - num2;
+							restador = codi - restador;
+							ascii2 = tope - restador;
+						} else {
+							ascii2 = ascii2 - num2;
+						}
+						solucio[i] = (char) ascii2;
+						cont = 3;
+						break;
+
+					case 3:
+						int ascii3 = (int) chars[i];
+
+						if ((ascii3 - num3) < 65) {
+							restador = ascii3 - num3;
+							restador = codi - restador;
+							ascii3 = tope - restador;
+						} else {
+							ascii3 = ascii3 - num3;
+						}
+						solucio[i] = (char) ascii3;
+						cont = 1;
+						break;
+
+					default:
+						break;
+					}
 				}
 			}
-			
+
 			String str = new String(solucio);
 			sortida.setText(str);
-			
+
 		}
-		
+
+	}
+
+	public void comprovarCaracters(char[] array) {
+
+		Pattern pattern = Pattern.compile("[A-Z]");
+		String comprovarMajuscules = new String(array);
+		if (entrada.getText().isEmpty()) {
+			System.out.println("Error: El text d'entrada no pot estar buit");
+			error = true;
+		} else if(!error){
+			for (int i = 0; i < array.length; i++) {
+				Matcher matcher = pattern.matcher(comprovarMajuscules);
+				boolean matchFound = matcher.find();
+
+				if (!matchFound && error == false) {
+					System.out.println("Error: El text d'entrada han de ser lletres majuscules i no ha contindre numeros");
+					error = true;
+				}
+				if (array[i] == ' ' && error == false) {
+					System.out.println("Error: El text d'entrada no pot tindre espais en blanc");
+					error = true;
+				}
+				if (array[i] == 'Ñ') {
+					array[i] = 'N';
+				}
+				if (array[i] == 'Ç') {
+					array[i] = 'C';
+				}
+			}
+		}
 	}
 
 }
